6946fd052e600913b4a14fdf289b3235b6b46ee7
Alishabaral5 <alishabara12345@gmail.com>
2025-11-01 20:14:21 +0545

    updated home page

--- homepage/views.py
+++ homepage/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    # context = {
-    #     "page_title": "Django Day 2",
-    #     "message": "Welcome to Day 2 of Django!"
-    # }
-    # return HttpResponse("Welcome to Django Workshop!")
     return render(request, 'homepage/home.html')
+    # return HttpResponse("Homepage is working!")
+
 def about(request):
     return HttpResponse("Welcome to about page.")
+
